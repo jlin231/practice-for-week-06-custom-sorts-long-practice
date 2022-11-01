4572a9cdb03c6f90e01c99d2ff52f89e068a07a8
jlin231 <jonlin981@gmail.com>
2022-11-01 13:04:24 -0700

    oddEvenSort

--- custom-sorts.js
+++ custom-sorts.js
@@ -1,69 +1,76 @@
 function ageSort(users) {
   let res = [];
   let count = {};
+
   for (let i = 0; i < users.length; i++) {
-    count[i] = users[i].age;
+    count[users[i].age] = i;
+    res.push(users[i].age);
   }
-  //count = { '0': 30, '1': 25, '2': 32, '3': 55 }
+  res.sort();
+  let output = []
+  for (let i = 0; i < res.length; i++) {
+    output.push(users[count[res[i]]])
+  }
+  console.log(output)
+  return output;
 }
 
-// function oddEvenSort(arr) {
-//   // Your code here
-// }
+function oddEvenSort(arr) {
+  // Your code here
+}
 
-// function validAnagrams(s, t) {
-//   // Your code here
-// }
+function validAnagrams(s, t) {
+  // Your code here
+}
 
-// function reverseBaseSort(arr) {
-//   // Your code here
-// }
+function reverseBaseSort(arr) {
+  // Your code here
+}
 
-// function frequencySort(arr) {
-//   // Your code here
-// }
+function frequencySort(arr) {
+  // Your code here
+}
 
-const users = [
-  {
-    id: 1,
-    firstName: "John",
-    lastName: "Doe",
-    age: 30,
-    occupation: "Software Engineer",
-    friends: [2, 3, 4],
-  },
-  {
-    id: 2,
-    firstName: "Jane",
-    lastName: "Doe",
-    age: 25,
-    occupation: "Data Scientist",
-    friends: [1, 4],
-  },
-  {
-    id: 3,
-    firstName: "Mary",
-    lastName: "Smith",
-    age: 32,
-    occupation: "UX Designer",
-    friends: [2, 4],
-  },
-  {
-    id: 4,
-    firstName: "James",
-    lastName: "Johnson",
-    age: 55,
-    occupation: "CTO",
-    friends: [1, 2, 3],
-  },
-];
-
-ageSort(users);
-
-// module.exports = [
-//   // oddEvenSort,
-//   // validAnagrams,
-//   // reverseBaseSort,
-//   // frequencySort,
-//   ageSort,
+// const users = [
+//   {
+//     id: 1,
+//     firstName: "John",
+//     lastName: "Doe",
+//     age: 30,
+//     occupation: "Software Engineer",
+//     friends: [2, 3, 4],
+//   },
+//   {
+//     id: 2,
+//     firstName: "Jane",
+//     lastName: "Doe",
+//     age: 25,
+//     occupation: "Data Scientist",
+//     friends: [1, 4],
+//   },
+//   {
+//     id: 3,
+//     firstName: "Mary",
+//     lastName: "Smith",
+//     age: 32,
+//     occupation: "UX Designer",
+//     friends: [2, 4],
+//   },
+//   {
+//     id: 4,
+//     firstName: "James",
+//     lastName: "Johnson",
+//     age: 55,
+//     occupation: "CTO",
+//     friends: [1, 2, 3],
+//   },
 // ];
+
+
+module.exports = [
+  oddEvenSort,
+  validAnagrams,
+  reverseBaseSort,
+  frequencySort,
+  ageSort,
+];
